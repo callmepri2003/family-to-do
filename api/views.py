91551a15a78134cc11d5c326d580dfa4e69c143e
callmepri <140776053+callmepri@users.noreply.github.com>
2024-12-10 12:50:31 +1100

    feat: Readied the system for frontend authentication

--- api/views.py
+++ api/views.py
@@ -7,6 +7,10 @@ from rest_framework import permissions, viewsets, status
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 class TaskViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -14,6 +18,20 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Optionally filter tasks by a `username` query parameter.
+        """
+        username = self.request.query_params.get('username', None)
+        if username:
+            family_member_model = FamilyMember.objects.get(username=username)  # Retrieve the User model (FamilyMember in this case)
+            family_member = FamilyMember.objects.filter(username=username).first()
+            if family_member:
+                return self.queryset.filter(assigned_to=family_member)  # Replace `assigned_to` with the actual task field
+            else:
+                return self.queryset.none()
+        return self.queryset
 
     @action(detail=False, methods=['POST'])
     def assign_task(self, request, pk=None):
@@ -56,3 +74,10 @@ class UserViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['GET'])
     def getAllTasks():
         return 
+
+class LoginAPIView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        return Response({"Success": "Login Successful", "User": request.user.username, "userId": request.user.id}, status=status.HTTP_200_OK)
